cfbd270c715176023b07105d51a26ea2d15777a4
EvanSC <evangundala@gmail.com>
2025-02-16 09:23:29 +0700

    setup database,orm prisma,components layout

--- src/app/jobs/[job]/page.tsx
+++ src/app/jobs/[job]/page.tsx
@@ -1,7 +1,21 @@
-export default function Job() {
+import db from "@/lib/db";
+
+interface JobProps {
+  params: {
+    job : string;
+  };
+}
+
+
+export default async function Job({ params }: JobProps) {
+  const { job } = await params;
+
+
+
   return (
     <div>
       <div>Test Job</div>
+      <div>{job}</div>
     </div>
   );
 }
---
+++ src/components/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ChildrenProps } from '@/types'
+
+
+const layout = ({children} : ChildrenProps) => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      {children}
+    </div>
+  )
+}
+
+export default layout
