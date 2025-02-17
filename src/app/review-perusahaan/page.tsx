27a3d1c32de71d4408d1c7fcdad43a792a8df799
EvanSC <evangundala@gmail.com>
2025-02-17 23:17:23 +0700

    navbar:navbar responsive

---
+++ src/app/review-perusahaan/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Hallo ini review perusahaan</h1>
+    </div>
+  )
+}
+
+export default page
--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 
 const NavLink: React.FC<{ href: string, children: React.ReactNode }> = ({ href, children }) => {
+  const pathname = usePathname();
   return (
-    <Link className="border px-3 py-1 border-primary_color rounded-full" href={href}>
+    <Link className={`border px-3 py-[1px] border-primary_color rounded-full text-center text-sm lg:text-base
+                      ${pathname === href ? "bg-primary_color text-white" : "text-primary_color"}`} href={href}>
       {children}
     </Link>
   );
