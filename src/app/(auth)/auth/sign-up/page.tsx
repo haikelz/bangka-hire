1e88b587511b81d4c5609e4dda01ef899663cb49
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-02-17 20:47:26 +0700

    fix: resolve next-auth typescript error

---
+++ src/app/(auth)/auth/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function SignUp() {
+  return (
+    <div>
+      <div>
+        <Button>Sign Up</Button>
+      </div>
+    </div>
+  );
+}
