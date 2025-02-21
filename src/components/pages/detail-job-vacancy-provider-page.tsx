0ef162c2963e805ea1ea01c7e3362a460b504ca7
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-02-21 07:59:49 +0700

    feat: finished detail job vacancy provider and make it responsive

--- src/components/pages/detail-job-vacancy-provider-page.tsx
+++ src/components/pages/detail-job-vacancy-provider-page.tsx
@@ -2,20 +2,24 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/hooks/use-toast";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useForm } from "react-hook-form";
-
 import { useUser } from "@/hooks/use-current-user";
+import { toast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
+import { ratingAtom } from "@/store";
 import { UserProps } from "@/types";
-import { atom, useAtom } from "jotai";
-import { FacebookIcon, InstagramIcon, MailIcon, StarIcon } from "lucide-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAtom } from "jotai";
+import {
+  Calendar,
+  FacebookIcon,
+  InstagramIcon,
+  MailIcon,
+  StarIcon,
+} from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import CardResultJob from "../card-result-job";
-
-const ratingAtom = atom<number>(0);
 
 export function DetailJobVacancyProviderPage() {
   const user = useUser() as unknown as UserProps;
@@ -33,7 +37,7 @@ export function DetailJobVacancyProviderPage() {
     register,
   } = useForm({
     defaultValues: {
-      review: "",
+      comment: "",
     },
   });
 
@@ -69,20 +73,51 @@ export function DetailJobVacancyProviderPage() {
   return (
     <>
       <div className="rounded-sm bg-secondary_color_2 px-7 py-6">
-        <div>
-          <div className="flex space-y-0.5 flex-col justify-between items-center w-fit">
-            <div className="flex space-x-2 justify-between items-center w-fit">
-              <div className="flex space-x-2 justify-center items-center w-fit">
-                <StarIcon
+        <div className="space-y-5">
+          <div className="flex sm:flex-row flex-col sm:space-x-10 space-y-2 sm:space-y-0 justify-between items-start w-fit">
+            <Image
+              src="/assets/logo.png"
+              alt="company logo"
+              width={77}
+              height={77}
+            />
+            <div className="space-y-1">
+              <h3 className="font-bold text-xl">HiRex</h3>
+              <div className="flex space-x-4 justify-center items-center w-fit">
+                <div className="flex justify-center items-center w-fit space-x-2">
+                  <span>4.8</span>
+                  <StarIcon
+                    width={16}
+                    height={16}
+                    className="fill-secondary_color_1 stroke-secondary_color_1"
+                  />
+                  <p>Total Penilaian</p>
+                </div>
+                <p className="underline">16 Ulasan</p>
+              </div>
+              <div className="space-x-1 flex justify-center items-center w-fit">
+                <Image
+                  src="/assets/location-company.svg"
+                  alt="location"
                   width={16}
                   height={16}
-                  className="fill-secondary_color_1 stroke-secondary_color_1"
                 />
-                <p>Total Penilaian</p>
+                <p>Web dan Teknologi </p>
               </div>
-              <p>Ulasan</p>
+              <div className="space-x-1 flex justify-center items-center w-fit">
+                <Image
+                  src="/assets/fluent-location.svg"
+                  alt="location"
+                  width={16}
+                  height={16}
+                />
+                <p>Bangka Induk, Bangka Belitung</p>
+              </div>
+              <div className="flex justify-center items-center space-x-2 w-fit">
+                <Calendar className="" width={16} height={16} />
+                <p>Bergabung sejak Desember 2024</p>
+              </div>
             </div>
-            <p>Bergabung sejak</p>
           </div>
           <div>
             <div className="border-b w-full border-b-primary_color py-3">
@@ -109,11 +144,24 @@ export function DetailJobVacancyProviderPage() {
             </div>
             {companyTab === "deskripsi" ? (
               <div className="w-full py-7">
-                <p className="font-bold">Tentang Perusahaan</p>
+                <div className="space-y-2">
+                  <p className="font-bold">Tentang Perusahaan</p>
+                  <p className="text-justify">
+                    HireX adalah Perusahaan yang bergerak di bidang web dan
+                    teknologi. saat ini sedang membutuhkan UI/UX Desainer
+                    sebagai pendukung tim dibidang web dan teknologi Lorem ipsum
+                    dolor sit amet consectetur. Donec porta sem netus diam
+                    fermentum porta amet elit. Adipiscing elementum suspendisse
+                    pulvinar enim proin ornare fringilla ullamcorper adipiscing.
+                  </p>
+                </div>
                 <div className="mt-6">
                   <h3 className="text-black text-xl font-bold">Hubungi Kami</h3>
                   <div className="space-y-3">
-                    <p className="font-bold text-black mt-3">Alamat</p>
+                    <div className="space-y-2">
+                      <p className="font-bold text-black mt-3">Alamat</p>
+                      <p>Jl. Ahmad Yani, No sekian dari 11/9</p>
+                    </div>
                     <div className="space-y-2">
                       <p className="font-bold text-black">Media Sosial</p>
                       <div className="flex justify-center items-center space-x-2 w-fit">
@@ -145,14 +193,14 @@ export function DetailJobVacancyProviderPage() {
                         </button>
                       ))}
                   </div>
-                  <div className="flex justify-center items-center w-full space-x-8 mt-4 mb-7">
+                  <div className="flex justify-center items-center w-full flex-col md:space-y-0 md:flex-row md:space-x-8 mt-4 mb-7 space-y-3">
                     <Input
                       placeholder="Tulis Ulasan"
                       className="border border-primary_color focus:border-primary_color text-black"
                     />
                     <Button
                       type="submit"
-                      className="border border-primary_color bg-[#F3F9FF] rounded-sm"
+                      className="border border-primary_color bg-[#F3F9FF] rounded-sm md:w-fit w-full"
                       size="lg"
                       variant="outline"
                     >
@@ -162,10 +210,11 @@ export function DetailJobVacancyProviderPage() {
                 </form>
                 <div className="flex w-full flex-col justify-start items-start">
                   <div className="border-primary_color border px-4 bg-white py-4 w-full rounded-sm">
-                    <div>
+                    <div className="space-y-1">
                       <div className="flex w-full justify-between items-start">
                         <div className="flex justify-center items-center w-fit space-x-2">
                           <Image
+                            className="rounded-full"
                             src="/assets/logo.png"
                             alt="comment"
                             width={50}
@@ -175,7 +224,7 @@ export function DetailJobVacancyProviderPage() {
                         </div>
                         <p>12.05</p>
                       </div>
-                      <div className="flex justify-center items-center w-fit space-x-2">
+                      <div className="flex justify-center items-center w-fit space-x-1">
                         <span>4</span>
                         <StarIcon
                           width={16}
@@ -183,7 +232,7 @@ export function DetailJobVacancyProviderPage() {
                           className="fill-secondary_color_1 stroke-secondary_color_1"
                         />
                       </div>
-                      <p>
+                      <p className="text-justify">
                         ipsum dolor sit amet consectetur. Donec porta sem netus
                         diam fermentum porta amet elit.
                       </p>
