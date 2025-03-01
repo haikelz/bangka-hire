3c8eb41b7c1aa457b43dc75a6f23c56a8dea4ba7
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-03-01 19:13:02 +0700

    refactor(components): using select from shadcn instead of default select in homepage

--- src/components/jobs/form-search-job.tsx
+++ src/components/jobs/form-search-job.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { citiesList } from "@/lib/static";
 import { searchJob, valueFilterCity, valueFilterSalary } from "@/store";
 import { useAtom } from "jotai";
-import { ChevronDown } from "lucide-react";
 import { FormEvent, useEffect, useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export default function FormSearchJob() {
   const [openSelect, setOpenSelect] = useState<string | null>(null);
@@ -57,67 +64,44 @@ export default function FormSearchJob() {
 
       {/* Select untuk filter lokasi */}
       <div ref={locationRef} className="relative w-full md:w-[20%]">
-        <select
-          className="appearance-none w-full rounded-lg p-2 border border-gray-300 focus:outline-none cursor-pointer md:text-xs lg:text-base"
-          onClick={(e) => {
-            e.stopPropagation(); // mencegah event bubbling
-            setOpenSelect(openSelect === "location" ? null : "location");
-          }}
-          name="location"
-          id="location"
-          onChange={(e) => setFilterCity(e.target.value)}
+        <Select
+          onValueChange={(value) => setFilterCity(value)}
+          defaultValue="Pilih Lokasi"
         >
-          <option value="" selected disabled>
-            Pilih Lokasi...
-          </option>
-          <option value="Pangkal Pinang">Pangkal Pinang</option>
-          <option value="Sungailiat">Sungailiat</option>
-          <option value="Koba">Koba</option>
-          <option value="Toboali">Toboali</option>
-          <option value="Muntok">Muntok</option>
-          <option value="Tanjung Pandan">Tanjung Pandan</option>
-          <option value="Manggar">Manggar</option>
-        </select>
-
-        {/* Custom Icon */}
-        <div
-          className={`absolute top-1/2 right-3 transform -translate-y-1/2 transition-transform duration-300 ${
-            openSelect === "location" ? "rotate-180" : ""
-          }`}
-        >
-          <ChevronDown className="w-6 h-6 text-black" />
-        </div>
+          <SelectTrigger className="bg-white">
+            <SelectValue placeholder="Pilih Lokasi" className="text-black" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem disabled value="Pilih Lokasi">
+              Pilih Lokasi
+            </SelectItem>
+            {citiesList.map((item) => (
+              <SelectItem key={item.id} value={item.value}>
+                {item.value}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </div>
 
       {/* Select untuk filter urutan gaji */}
       <div ref={salaryRef} className="relative w-full md:w-[20%]">
-        <select
-          className="appearance-none w-full rounded-lg p-2 border border-gray-300 focus:outline-none cursor-pointer md:text-xs lg:text-base"
-          onClick={(e) => {
-            e.stopPropagation(); // mencegah event bubbling
-            setOpenSelect(openSelect === "salary" ? null : "salary");
-          }}
-          name="salary"
-          id="salary"
-          onChange={(e) => setFilterSalary(e.target.value)}
+        <Select
+          onValueChange={(value) => setFilterSalary(value)}
+          defaultValue="Urutkan Gaji"
         >
-          <option value="" selected disabled>
-            Urutkan Gaji...
-          </option>
-          <option value="Tertinggi">Tertinggi</option>
-          <option value="Terendah">Terendah</option>
-        </select>
-
-        {/* Custom Icon */}
-        <div
-          className={`absolute top-1/2 right-3 transform -translate-y-1/2 transition-transform duration-300 ${
-            openSelect === "salary" ? "rotate-180" : ""
-          }`}
-        >
-          <ChevronDown className="w-6 h-6 text-black" />
-        </div>
+          <SelectTrigger className="bg-white">
+            <SelectValue placeholder="Urutkan Gaji" className="text-black" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem disabled value="Urutkan Gaji">
+              Urutkan Gaji
+            </SelectItem>
+            <SelectItem value="Tertinggi">Tertinggi</SelectItem>
+            <SelectItem value="Terendah">Terendah</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-
       <Button className="w-full md:w-[10%] bg-secondary_color_2 text-black hover:bg-secondary_color_3">
         Cari
       </Button>
